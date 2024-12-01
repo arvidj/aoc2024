51deab11b5742ef52e6ca3e2b3cc4109cb865634
Arvid Jakobsson <arvid.jakobsson@ahrefs.com>
2024-12-01 13:28:09 +0100

    add ex1

---
+++ bin/ex1.ml
@@ -0,0 +1,36 @@
+open Aoc2024
+
+let () =
+  match Sys.argv with
+  | [| _; path |] ->
+      let left, right =
+        Base.fold_lines ~path ([], []) (fun (left, right) line ->
+            Scanf.sscanf line "%d   %d" @@ fun l r -> (l :: left, r :: right))
+      in
+      let left, right =
+        (List.sort Int.compare left, List.sort Int.compare right)
+      in
+      let diff_sum =
+        List.fold_left2
+          (fun diff_sum l r -> diff_sum + abs (l - r))
+          0 left right
+      in
+      pfn "Aoc2024 ex1.b) Diff sum: %d" diff_sum
+  | [| _; "--part2"; path |] ->
+      let left, right =
+        Base.fold_lines ~path ([], []) (fun (left, right) line ->
+            Scanf.sscanf line "%d   %d" @@ fun l r -> (l :: left, r :: right))
+      in
+      (* [occ v] Number of occurrences of [v] in right list. *)
+      let occ : int -> int =
+        let occ : (int, int) Hashtbl.t = Hashtbl.create (List.length right) in
+        List.iter (fun d -> hashtbl_update ~default:0 occ d succ) right;
+        fun d -> hashtbl_get ~default:0 occ d
+      in
+      let similarity_score =
+        List.fold_left (fun acc l -> acc + (l * occ l)) 0 left
+      in
+      pfn "Aoc2024 ex1.a) Similarity score: %d" similarity_score
+  | _ ->
+      Printf.eprintf "Usage: %s [--part2] PATH_INPUT" Sys.argv.(0);
+      exit 1
